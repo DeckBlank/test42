8ce7dbc4fbc8653b3ead30b541594160eb72cc75
Jose Luciano Guzman Santiago <jguzman@dcanje.com>
2021-06-16 23:30:44 -0500

    creando test with mocha

--- resourcesTest/axiosRequest.js
+++ resourcesTest/axiosRequest.js
@@ -1,35 +1,33 @@
 "use strict"
-import axios from 'axios';
+const axios = require('axios');
 
-export const request = async ({url,method,data,headers}) =>{
+const request = async ({url,method,data,headers}) =>{
     if(typeof data!="string") data = JSON.stringify(data);
+    if(method==='get'){
+        headers = {}
+    }else if(method==='post'){
+        headers = {
+            ...headers,
+            'Content-Type': 'application/json'
+        }
+    }
     var config = {
         method,
         url,
-        headers: headers?headers:{ 
-          'Content-Type': 'application/json'
-        },
+        headers,
         data
       };
     try {
         let respuesta = await axios(config)
         return respuesta.data
     } catch (error) {
-        console.log(error);
+        console.log('error');
         return null
     }
 }
 
 
-console.log(request({
-    method: 'post',
-    url: 'localhost:8080/auth/register',
-    headers: { 
-      'Content-Type': 'application/json'
-    },
-    data : {
-        "nombre": "josejose",
-        "password": "josejose"
-    }
-}));
 
+
+
+module.exports = request;
---
+++ test/testApiProductos.js
@@ -0,0 +1,81 @@
+const request = require('../resourcesTest/axiosRequest')
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('axios',()=>{
+    let idCreated = null;
+    ///Leemos los productos que ya existen en la db
+    it('getProductos',async ()=>{
+        let respuesta = await request({
+            method: 'get',
+            url: 'http://localhost:8080/api/productos',
+            headers: {},
+            data : ''
+        })
+        console.log(respuesta);
+        //evaluamos si nos retorna un objeto
+        expect(typeof respuesta).equal("object");
+    })
+    //Agregamos un nuevo producto y guardamos su ID
+    it('addProducto',async ()=>{
+        let respuesta = await request({
+            method: 'post',
+            url: 'http://localhost:8080/api/productos',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : {
+                title:"sdsd",
+                price:12,
+                tumbnails : "https://marketing4ecommerce.net/wp-content/uploads/2018/01/Depositphotos_3667865_m-2015-compressor.jpg"
+             }
+        })
+        idCreated = respuesta.id
+        console.log(respuesta);
+        //evaluamos si nos retorna un objeto
+        expect(typeof respuesta).equal("object");
+    })
+   /// Obtenemos el producto agregado con el ID guardado
+    it('getProductoById',async ()=>{
+        let respuesta = await request({
+            method: 'get',
+            url: `http://localhost:8080/api/productos/${idCreated}`,
+            headers: {},
+            data : ''
+        })
+        console.log(respuesta);
+        //evaluamos si nos retorna un objeto
+        expect(typeof respuesta).equal("object");
+    })
+    /// Actualizamos el producto agregado
+    it('updateProductoById',async ()=>{
+        let respuesta = await request({
+            method: 'put',
+            url: `http://localhost:8080/api/productos/${idCreated}`,
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : {
+                id:3,
+                title:"nuevo",
+                tumbnails:"https://marketing4ecommerce.net/wp-content/uploads/2018/01/Depositphotos_3667865_m-2015-compressor.jpg",
+                price:14444
+              }
+        })
+        console.log(respuesta);
+        //evaluamos si nos retorna un objeto
+        expect(typeof respuesta).equal("object");
+    })
+    /// Eliminamos el producto agregado
+    it('deleteById',async ()=>{
+        let respuesta = await request({
+            method: 'delete',
+            url: `http://localhost:8080/api/productos/${idCreated}`,
+            headers: {},
+            data : ''
+        })
+        console.log(respuesta);
+        //evaluamos si nos retorna un objeto
+        expect(typeof respuesta).equal("object");
+    })
+})
